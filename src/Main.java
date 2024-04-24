834f4caec172708d7489d7d041104b9466de146f
pnevmoslon1 <rinatv305@gmail.com>
2024-04-24 08:46:12 +0400

    lab2

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+//https://habr.com/ru/articles/438512/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Lavenshtein.countOfOperations( "Гибралтар","Лабрадор"));
+    }
+}
