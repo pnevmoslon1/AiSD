1890cd14d575036cbede130e6f3865c587ebec5a
pnevmoslon1 <rinatv305@gmail.com>
2024-05-22 03:34:43 +0400

    YellowKek

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,33 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(2);
+//        BinSearchTree tree = new BinSearchTree();
+//        tree.insert(33);
+//        tree.insert(5);
+//        tree.insert(4);
+//        tree.insert(17);
+//        tree.insert(35);
+//        tree.insert(99);
+////        System.out.println(tree.find(33));
+////        System.out.println(tree.find(1));
+//        tree.printTree(tree.root);
+//        System.out.println();
+//        tree.delete(5);
+//        tree.printTree(tree.root);
+//        System.out.println();
+
+
+        Graph g = new Graph(4);
+
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 2);
+        g.addEdge(2, 0);
+        g.addEdge(2, 3);
+        g.addEdge(3, 3);
+
+
+
+        g.BFS(2);
     }
 }
