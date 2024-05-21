a37e5304c0bcfa5d54e91ec13d4007bb775b0298
pnevmoslon1 <rinatv305@gmail.com>
2024-05-22 00:01:17 +0400

    YellowKek

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
-//https://habr.com/ru/articles/438512/
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Lavenshtein.countOfOperations( "Гибралтар","Лабрадор"));
+        System.out.println(2);
     }
 }
