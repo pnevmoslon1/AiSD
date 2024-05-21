4a4fa7a1e989a714fd312296e4be5e6e5f8bdbf9
pnevmoslon1 <rinatv305@gmail.com>
2024-05-22 03:44:26 +0400

    YellowKek 3.0

--- src/Main.java
+++ src/Main.java
@@ -17,17 +17,27 @@ public class Main {
 //        System.out.println();
 
 
-        Graph g = new Graph(4);
+//        Graph g = new Graph(4);
+//
+//        g.addEdge(0, 1);
+//        g.addEdge(0, 2);
+//        g.addEdge(1, 2);
+//        g.addEdge(2, 0);
+//        g.addEdge(2, 3);
+//        g.addEdge(3, 3);
+//
+//        g.BFS(2);
 
-        g.addEdge(0, 1);
-        g.addEdge(0, 2);
-        g.addEdge(1, 2);
-        g.addEdge(2, 0);
-        g.addEdge(2, 3);
-        g.addEdge(3, 3);
 
-
-
-        g.BFS(2);
+        int INF = 999999999;
+        int graph[][] = {
+                {0, 5, INF, 10},
+                {INF, 0, 3, INF},
+                {INF, INF, 0, 1},
+                {INF, INF, INF, 0}
+        };
+        FloydWarshall a = new FloydWarshall();
+        a.floydWarshall(graph);
     }
+
 }
