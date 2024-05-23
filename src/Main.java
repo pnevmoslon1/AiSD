a9cd75317c995c38649840d8d65b092e3e40feea
pnevmoslon1 <rinatv305@gmail.com>
2024-05-23 13:17:07 +0400

    YellowKek 1.0

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,39 @@
-//https://habr.com/ru/articles/438512/
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Lavenshtein.countOfOperations( "Гибралтар","Лабрадор"));
+//        HashTableKv table = new HashTableKv(10);
+//
+//        table.insert("Elena", "Golovach");
+//        table.insert("Slava", "Marlow");
+//        table.insert("Vitya", "Otvertka");
+//
+//        String value = table.get("Elena");
+//        System.out.println("Значение по ключу 'Elena': " + value);
+//
+
+//        HashTable hashTable = new HashTable();
+//        hashTable.put(new int[]{1}, 100);
+//        hashTable.put(new int[]{2}, 200);
+//        hashTable.put(new int[]{3}, 300);
+//
+//        System.out.println(hashTable.get(new int[]{1}));
+//        System.out.println(hashTable.get(new int[]{2}));
+//        System.out.println(hashTable.get(new int[]{3}));
+//
+//        hashTable.remove(new int[]{2});
+//        System.out.println(hashTable.get(new int[]{2}));
+//
+//
+//
+//
+//        BloomFilter bloomFilter = new BloomFilter(15);
+//
+//        bloomFilter.add("rinat");
+//        bloomFilter.add("apple");
+//        bloomFilter.add("john");
+//        bloomFilter.add("asd");
+//
+//        System.out.println(bloomFilter.contains("rinat"));
+//        System.out.println(bloomFilter.contains("max"));
+//        System.out.println(bloomFilter.contains("bimbimbambam"));
     }
 }
